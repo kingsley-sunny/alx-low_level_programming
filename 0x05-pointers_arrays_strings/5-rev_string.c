9c378be1b5ed6ebff808af8c3e94f152c5ef870b
Ezeobi Kingsley Sunny <ezeobisunny51@gmail.com>
2022-09-19 18:36:53 -0700

    fixing task 5

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,19 +1,29 @@
 #include "main.h"
 /**
-* rev_string - prints a reverse string
-*@s: A pointer to an int that will be changed
-*
-*Return: void
+ * rev_string - prints reversed string, followed by a new line
+ * @s: pointer to the string to print
+ * Return: void
 */
 
-int main(void)
+void rev_string(char *s)
 {
-    char s[10] = "123456";
 
-    printf("%s\n", s);
-    rev_string(s);
-    printf("%s\n", s);
-    return (0);
+int len, i, half;
+char temp;
+
+for (len = 0; s[len] != '\0'; len++)
+;
+
+i = 0;
+
+half = len / 2;
+
+while (half--)
+{
+	temp = s[len - i - 1];
+	s[len - i - 1] = s[i];
+	s[i] = temp;
+	i++;
 }
 
-
+}
