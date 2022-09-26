71d976b6d9ab5449715376dd98d01954e5222cb8
kingsley-sunny <ezeobisunny51@gmail.com>
2022-09-26 15:56:35 -0700

    done with task 0

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+*_memset - Fills memory with a constant byte
+*
+*@s: the memory
+*@b: constant byte
+*@n: number of the byte
+*Return: points to a pointer
+*/
+
+char *_memset(char *s, char b, unsigned int n)
+{
+unsigned int i = 0;
+
+while (i < n)
+{
+s[i] = b;
+i++;
+}
+
+return (s);
+}
