26d59427a7bfd3e3b2642123acec9fb6002c1099
kingsley-sunny <ezeobisunny51@gmail.com>
2022-09-26 18:49:16 -0700

    done with task 1 till task 5

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+*_strchr - Finds a char in a string
+*@s: the memory
+*@c: constant byte
+*Return: points to the c
+*/
+
+char *_strchr(char *s, char c)
+{
+unsigned int i;
+
+i = 0;
+while (s[i] != '\0')
+{
+if (s[i] == c)
+{
+break;
+}
+i++;
+}
+return (s[i] == c ? (s + i) : '\0');
+}
